f5a13cbc0b42c40217bf5eea4982c8c9b0941d23
Francesco <francesco-lion@virgilio.it>
2026-03-11 12:52:24 +0100

    chore: check is valid user in core/server

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,6 +3,17 @@ import { type Express }  from 'express';
 import express from 'express';
 import {decryptJsonTokenData} from '@my-books/core';
 
+type ValidUser = {
+  sub: string,
+  given_name: string
+}
+const isValidUser = (user: ValidUser) => {
+  return user &&
+    typeof user.sub === 'string' &&
+    /^\d+$/.test(user.sub) &&
+    typeof user.given_name === 'string' &&
+    /^\w+$/.test(user.given_name);
+}
 const app: Express = express();
 app.use(express.json());
 app.use((req, res, next) => {
@@ -11,7 +22,7 @@ app.use((req, res, next) => {
   const token = tokenFromHeader!.startsWith('Bearer ') ? tokenFromHeader!.toString().split(' ')[1] : undefined;
   if (typeof token !== 'string') return res.status(400).json('token should be present in the header');
   const decodedData = decryptJsonTokenData(token!);
-  if (!decodedData) return res.status(403).json('user data is not wrong');
+  if (!decodedData || !isValidUser(decodedData as ValidUser)) return res.status(403).json('user data is not wrong');
   res.locals.user = decodedData;
   next();
 });
