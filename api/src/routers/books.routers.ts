e839b8b8f5d984691e632a4c969ea9b11d998a67
Francesco <francesco-lion@virgilio.it>
2026-03-17 16:44:30 +0100

    chore: added new routes for books.routers

--- api/src/routers/books.routers.ts
+++ api/src/routers/books.routers.ts
@@ -5,15 +5,13 @@ import { cleanParam } from "../utils/helper";
 import SelectQueryBuilder from "../db/builder/select-query.builder";
 import InsertQueryBuilder from "../db/builder/insert-query.builder";
 import DbService from "../services/db.service";
+import { ValidUser } from "../types/user";
 
-
+const imgMiddleware = multer({ storage: multer.memoryStorage() }).single('img');
 const booksRouters: Router = Router();
+
 booksRouters.get('/books/get', async (req, res) => {
-  const { author, title } = req.query;
-  const books = await getBooks(
-    cleanParam(author as string),
-    cleanParam(title as string)
-  );
+  const books = await getBooks();
   res.status(200).json(books);
 });
 
@@ -33,38 +31,80 @@ booksRouters.get('/books/:id', async (req, res) => {
   res.status(200).json(book.rows[0]);
 });
 
-booksRouters.post('/books/add', async (req, res) => {
+booksRouters.get('/books/user/:userId', async (req, res) => {
+  const id = Number(req.params.userId);
+  if (isNaN(id)) return res.status(400).json({msg: 'book id is not valid'});
+  const books = await getBooksByUserId(id);
+  if (books.length === 0) return res.status(404).json({msg: 'user have no books'});
+  res.status(200).json(books);
+});
+
+booksRouters.get('/books/filter/:author/:title', async (req, res) => {
+  const author = req.params.author;
+  const title = req.params.title;
+  const books = await getFilteredBooksByAuthorOrTitle(author, title);
+  if (books.length === 0) return res.status(404).json({msg: 'user have no books'});
+  res.status(200).json(books);
+});
+
+booksRouters.post('/books/add', imgMiddleware, async (req, res) => {
   try {
-    const { title, author, description, img } = req.body;
+    const user = res.locals.user as ValidUser;
+    const imgBuffer = req.file;
+    const { title, author, description } = req.body;
     const insert = new InsertQueryBuilder('books')
-      .withFields('title', 'author', 'description', 'img')
-      .withValues(title, author, description, img)
-      .withReturning('id', 'title', 'author', 'description', 'img')
+      .withFields('title', 'author', 'description', 'img', 'userId')
+      .withValues('$1', '$2', '$3', '$4')
+      .withReturning('id', 'title', 'author', 'description', 'img', 'userId')
       .build();
-    const book = await DbService.query(insert);
+    const book = await DbService.query(insert, [
+      title,
+      author,
+      description,
+      imgBuffer,
+      user.sub
+    ]);
     res.status(201).json(book.rows[0]);
   } catch(e: any) {
     res.status(500).json({msg: 'error adding the book'});
   }
 });
 
-async function getBooks(author?: string, title?: string): Promise<Book[]> {
+async function getBooks(): Promise<Book[]> {
   const select = new SelectQueryBuilder('books')
     .withFields('id', 'title', 'author', 'description', 'img')
-  if (shouldFilterByAuthorAndTitle(author, title)) {
-    select
-      .withWhere({
-        lOperand: 'author',
-        operator: 'LIKE',
-        rOperand: `%${author}%`,
-        rLogicOperand: 'OR'
-      })
-      .withWhere({
-        lOperand: 'UPPER(title)',
-        operator: 'LIKE',
-        rOperand: `%${title!.toUpperCase()}%`,
-      })
-  }
+  const data = await DbService.query(select.build());
+  return data.rows as Book[];
+}
+
+async function getBooksByUserId(userId: number): Promise<Book[]> {
+    const select = new SelectQueryBuilder('books')
+    .withFields('id', 'title', 'author', 'description', 'img')
+    .withWhere({
+      lOperand: 'userId',
+      operator: '=',
+      rOperand: userId.toString()
+    });
+  const data = await DbService.query(select.build());
+  return data.rows as Book[];
+}
+
+async function getFilteredBooksByAuthorOrTitle(author?: string, title?: string): Promise<Book[]> {
+  const select = new SelectQueryBuilder('books')
+    .withFields('id', 'title', 'author', 'description', 'img')
+  if (!shouldFilterByAuthorAndTitle(author, title)) return await getBooks();
+  select
+    .withWhere({
+      lOperand: 'author',
+      operator: 'LIKE',
+      rOperand: `%${author}%`,
+      rLogicOperand: 'OR'
+    })
+    .withWhere({
+      lOperand: 'UPPER(title)',
+      operator: 'LIKE',
+      rOperand: `%${title!.toUpperCase()}%`,
+    });
   const data = await DbService.query(select.build());
   return data.rows as Book[];
 }
