655c6bf9eab6e72c6b15da7c21b41c48dc190d20
Francesco <francesco-lion@virgilio.it>
2026-04-06 18:59:02 +0200

    chore: getEnvPath integration

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,7 +3,10 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './graphql/typeDefs.gql.js';
 import { getUserTokenHandler } from './handlers/core.handler.js';
+import dotenv from 'dotenv';
+import { getEnvPath } from './utils/helper.js';
 
+dotenv.config({ path: getEnvPath(import.meta.url), debug: true });
 const server = new ApolloServer({ typeDefs, resolvers: {} });
 const { url } = await startStandaloneServer(server, {
   context: async ({ req }) => await getUserTokenHandler(req),
