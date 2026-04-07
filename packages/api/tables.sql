4ff6e7efbef2470cdb788e6aa0ffed4304dc14b7
Francesco <francesco-lion@virgilio.it>
2026-04-07 23:52:45 +0200

    chore: fixed db dump

--- packages/api/tables.sql
+++ packages/api/tables.sql
@@ -1,99 +1,35 @@
 -- PostgreSQL database dump
-\restrict wEagl7tXdb10GqMb88YcORHJjimQkhGKS2TjSHyR0aaALvnBfxzw7tSf3FrrYEg
--- Dumped from database version 18.1 (Debian 18.1-1.pgdg13+2)
--- Dumped by pg_dump version 18.1 (Debian 18.1-1.pgdg13+2)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-SET default_tablespace = '';
-SET default_table_access_method = heap;
 
--- Name: books; Type: TABLE; Schema: public; Owner: root
+CREATE TABLE IF NOT EXISTS public.users (
+    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "gId" text NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL,
+    u_role integer CHECK (u_role IN (1, 2))
+);
+
 CREATE TABLE IF NOT EXISTS public.books (
-    id bigint NOT NULL,
+    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title text NOT NULL,
     author text NOT NULL,
     description text NOT NULL,
     image bytea,
-    "userId" bigint
+    "userId" bigint REFERENCES public.users(id)
 );
-ALTER TABLE public.books OWNER TO root;
 
--- Name: books_id_seq; Type: SEQUENCE; Schema: public; Owner: root
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_sequences WHERE schemaname = 'public' AND sequencename = 'books_id_seq') THEN
-        ALTER TABLE public.books ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-            SEQUENCE NAME public.books_id_seq
-            START WITH 1
-            INCREMENT BY 1
-            NO MINVALUE
-            NO MAXVALUE
-            CACHE 1
-        );
-    END IF;
-END $$;
-
--- Name: comments; Type: TABLE; Schema: public; Owner: root
 CREATE TABLE IF NOT EXISTS public.comments (
-    id bigint NOT NULL,
-    "userId" bigint NOT NULL,
-    "bookId" bigint NOT NULL,
+    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "userId" bigint NOT NULL REFERENCES public.users(id),
+    "bookId" bigint NOT NULL REFERENCES public.books(id),
     comment text NOT NULL
 );
-ALTER TABLE public.comments OWNER TO root;
 
--- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: root
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_sequences WHERE schemaname = 'public' AND sequencename = 'comments_id_seq') THEN
-        ALTER TABLE public.comments ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-            SEQUENCE NAME public.comments_id_seq
-            START WITH 1
-            INCREMENT BY 1
-            NO MINVALUE
-            NO MAXVALUE
-            CACHE 1
-        );
-    END IF;
-END $$;
-
--- Name: users; Type: TABLE; Schema: public; Owner: root
-CREATE TABLE IF NOT EXISTS public.users (
-    id bigint NOT NULL,
-    name text NOT NULL,
-    email text NOT NULL,
-    u_role integer,
-    CONSTRAINT u_role CHECK ((u_role = ANY (ARRAY[1, 2])))
-);
-ALTER TABLE public.users OWNER TO root;
-
--- Name: books books_pkey; Type: CONSTRAINT; Schema: public; Owner: root
-ALTER TABLE IF EXISTS public.books ADD CONSTRAINT IF NOT EXISTS books_pkey PRIMARY KEY (id);
-
--- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: root
-ALTER TABLE IF EXISTS public.comments ADD CONSTRAINT IF NOT EXISTS comments_pkey PRIMARY KEY (id);
-
--- Name: users id; Type: CONSTRAINT; Schema: public; Owner: root
-ALTER TABLE IF EXISTS public.users ADD CONSTRAINT IF NOT EXISTS id PRIMARY KEY (id);
-
--- Name: comments bookId; Type: FK CONSTRAINT; Schema: public; Owner: root
-ALTER TABLE IF EXISTS public.comments ADD CONSTRAINT IF NOT EXISTS "bookId" FOREIGN KEY ("bookId") REFERENCES public.books(id);
-
--- Name: books userId; Type: FK CONSTRAINT; Schema: public; Owner: root
-ALTER TABLE IF EXISTS public.books ADD CONSTRAINT IF NOT EXISTS "userId" FOREIGN KEY ("userId") REFERENCES public.users(id);
-
--- Name: comments userId; Type: FK CONSTRAINT; Schema: public; Owner: root
-ALTER TABLE IF EXISTS public.comments ADD CONSTRAINT IF NOT EXISTS "userId" FOREIGN KEY ("userId") REFERENCES public.users(id);
+ALTER TABLE IF EXISTS public.users OWNER TO root;
+ALTER TABLE IF EXISTS public.books OWNER TO root;
+ALTER TABLE IF EXISTS public.comments OWNER TO root;
 
 -- PostgreSQL database dump complete
-\unrestrict wEagl7tXdb10GqMb88YcORHJjimQkhGKS2TjSHyR0aaALvnBfxzw7tSf3FrrYEg
