e36dc39358e583152dcaebf30b18d8c7dbcefa40
Francesco <francesco-lion@virgilio.it>
2026-04-08 22:27:00 +0200

    chore: fix hook

---
+++ src/hook/useCreateUserIfNotExists.hook.tsx
@@ -0,0 +1,64 @@
+import { ErrorLike, gql } from '@apollo/client';
+import { useMutation } from '@apollo/client/react';
+import { encryptToWebToken, UserEntityModel } from '@my-books/core';
+import { useEffect } from 'react';
+
+type CreareOrGetUserResponse = {
+  createUserIfNotExists: UserEntityModel
+} | null | undefined;
+
+const createOrGetMutation = gql`
+  mutation CreateOrGetUser {
+    createUserIfNotExists {
+      ... on User {
+        email
+        id
+        name
+        role
+      }
+      ... on ErrorResponse {
+        code
+        message
+      }
+    }
+  }
+`;
+
+export default function useCreateUserIfNotExists(
+  userToken: string | null,
+  setUserState: (user: UserEntityModel) => void
+): {
+  loading: boolean,
+  error: ErrorLike | undefined,
+  data: CreareOrGetUserResponse
+} {
+  const [createOrGetUser, { loading, data, error }] = useMutation<CreareOrGetUserResponse>(createOrGetMutation, {
+    context: {
+      authorization: 'Bearer ' + userToken
+    }
+  });
+
+  useEffect(() => {
+    if (userToken)
+      createOrGetUser();
+  },
+    [userToken, createOrGetUser]
+  );
+
+  useEffect(() => {
+    if (!data) return;
+    const user = data.createUserIfNotExists;
+    const entityJsonString = JSON.stringify(user);
+    const entityToken = encryptToWebToken(entityJsonString);
+    sessionStorage.setItem('userToken', entityToken);
+    setUserState(user);
+  },
+    [data, setUserState]
+  );
+
+  return {
+    loading,
+    data,
+    error
+  };
+}
